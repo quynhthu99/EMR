859f278fb0446be3eaa01cbb503e4fa6ed8e2282
quynhthu99 <quynhthuluong@gmail.com>
2022-11-01 13:58:01 +0700

    01_11_22

--- src/views/signManage/columns.ts
+++ src/views/signManage/columns.ts
@@ -3,7 +3,7 @@ export default [
     { title: 'Mã hồ sơ', dataIndex: 'fileId', key: 'fileId' },
     { title: 'Họ và tên', dataIndex: 'patient', key: 'patient' },
     { title: 'Người trình', dataIndex: 'ownerName', key: 'ownerName' },
-    { title: 'Thời gian tạo', dataIndex: 'created', key: 'created' },
+    { title: 'Thời gian tạo ', dataIndex: 'created', key: 'created' },
     // { title: 'Người tạo', dataIndex: 'owner', key: 'owner' },
     { title: 'Tùy chọn',  key: 'action' },
 ];
